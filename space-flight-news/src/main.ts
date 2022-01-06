7c9e173497a7819efe9e7caf1bbf1e57621f7afa
Arthur Gregório <arthurgregorioleal@gmail.com>
2022-01-06 13:05:06 -0300

    :pencil: OpenAPI: Swagger Support Implemented

--- space-flight-news/src/main.ts
+++ space-flight-news/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  Logger.log(
+    dotenv.config().error ? 'Dotenv file not found.' : 'Using dotenv.',
+    'Environment',
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Space Flight News')
+    .setDescription('Back-end Challenge 🏅 2021 - Space Flight News')
+    .setVersion('1.0')
+    .addTag('flight')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
