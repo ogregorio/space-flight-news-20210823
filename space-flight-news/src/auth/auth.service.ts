8e05b120da1722794f7d4475384147c4ee65cc4e
Arthur Gregório <arthurgregorioleal@gmail.com>
2022-01-08 14:14:08 -0300

    :lock: Security: Implemented password hashing

--- space-flight-news/src/auth/auth.service.ts
+++ space-flight-news/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import SHA3 from 'sha3';
 
 @Injectable()
 export class AuthService {
@@ -11,7 +12,7 @@ export class AuthService {
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne(username);
-    if (user && user.password === pass) {
+    if (user && user.password === new SHA3(512).update(pass).digest('hex')) {
       const { password, ...result } = user;
       return result;
     }
