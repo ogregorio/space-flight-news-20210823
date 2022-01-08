72095d0a3b98d27b72b1e11d0f4bebd0f3cdf3ba
Arthur Gregório <arthurgregorioleal@gmail.com>
2022-01-08 12:12:39 -0300

    :white_check_mark: Test: Created unit tests for users service

--- space-flight-news/src/users/users.service.spec.ts
+++ space-flight-news/src/users/users.service.spec.ts
@@ -1,18 +1,34 @@
+import { UserDto } from './dto/user.dto';
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import * as dotenv from 'dotenv';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
+    dotenv.config();
     const module: TestingModule = await Test.createTestingModule({
       providers: [UsersService],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    try {
+      service.create('teste', 'testepass');
+    } catch (e) {}
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('findOne - teste user', () => {
+    it('should find one user', async () => {
+      const response: UserDto = await service.findOne('teste');
+      expect(response.username).toBe('teste');
+    });
+  });
+
+  describe('findOne - user not found', () => {
+    it('should find one user', async () => {
+      const response = await service.findOne(Date.now().toString());
+      expect(response).toMatchObject({});
+    });
   });
 });
